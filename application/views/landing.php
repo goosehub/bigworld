0c19f77aa7e7bdcd627b4d036706a14612138aef
Goose <goosemixes@gmail.com>
2019-05-16 20:52:25 -0400

    Style Tweaks
    Haha, so you think your shtoyle can beat my shtoyle?

--- application/views/landing.php
+++ application/views/landing.php
@@ -10,43 +10,56 @@ body {
       
     </div>
     <div class="col-md-4">
-      
-      <h1><?php echo site_name(); ?></h1>
+      <br>
+      <br>
+      <div class="well">
 
-      <p class="lead">
-        Create your own world of chat
-      </p>
+        <h1><?php echo site_name(); ?></h1>
 
-      <h2>Worlds</h2>
-      <ul>
-        <?php foreach ($worlds as $world) { ?>
-        <li><?php echo $world['slug']; ?></li>
+        <p class="lead">
+          Create your own world of chat
+        </p>
+
+        <h2>
+          <span class="glyphicon glyphicon-globe" aria-hidden="true"></span>
+          Worlds
+        </h2>
+        <ul>
+          <?php foreach ($worlds as $world) { ?>
+          <li>
+            <a href="<?=base_url()?>/<?php echo $world['slug']; ?>">
+              <?php echo $world['slug']; ?>
+            </a>
+          </li>
+          <?php } ?>
+        </ul>
+
+        <!-- Login and Join -->
+        <?php if (!$user) { ?>
+        <button class="login_button menu_element btn btn-primary">
+            <i class="fa fa-sign-in" aria-hidden="true"></i>
+            Login
+        </button>
+        <button class="register_button menu_element btn btn-action">
+            <i class="fa fa-user-plus" aria-hidden="true"></i>
+            Join
+        </button>
         <?php } ?>
-      </ul>
 
-      <!-- Login and Join -->
-      <?php if (!$user) { ?>
-      <button class="login_button menu_element btn btn-primary">
-          <i class="fa fa-sign-in" aria-hidden="true"></i>
-          Login
-      </button>
-      <button class="register_button menu_element btn btn-action">
-          <i class="fa fa-user-plus" aria-hidden="true"></i>
-          Join
-      </button>
-      <?php } ?>
+        <?php if ($user) { ?>
+        <h2>Create World</h2>
 
-      <?php if ($user) { ?>
-      <h2>Create World</h2>
+        <form class="form-inline" id="create_world_form" action="<?=base_url()?>create_world" method="post">
+          <div class="form-group">
+            <label for="world_slug">World Slug</label>
+            <input type="text" name="slug" class="form-control" id="world_slug" placeholder="">
+          </div>
+          <button type="submit" class="btn btn-action">A Whole New World</button>
+        </form>
+        <?php } ?>
+        <br>
 
-      <form class="form-inline" id="create_world_form" action="<?=base_url()?>create_world" method="post">
-        <div class="form-group">
-          <label for="world_slug">World Slug</label>
-          <input type="text" name="slug" class="form-control" id="world_slug" placeholder="">
-        </div>
-        <button type="submit" class="btn btn-action">A Whole New World</button>
-      </form>
-      <?php } ?>
+      </div>
 
     </div>
     <div class="col-md-4">
